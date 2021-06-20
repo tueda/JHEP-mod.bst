a5f374751cf2ae18e794d6050f10b67aa32487c8
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-06-21 01:01:30 +0900

    docs: add images generated by the demo

--- demo/latex.mk
+++ demo/latex.mk
@@ -6,5 +6,12 @@ BUILDDIR = .build
 PREREQUISITE += JHEP.bst
 CLEANFILES += JHEP.bst
 
+PDFTOPPM_OPT += -r 150 -hide-annotations
+
 JHEP.bst:
 	$(download) JHEP.bst https://raw.githubusercontent.com/tueda/JHEP-mod.bst/upstream/JHEP.bst
+
+# "make conv" generates images for the demo.
+conv: demo-JHEP.png demo-JHEP-mod.png
+	convert -trim demo-JHEP.png ../docs/images/demo-JHEP.png
+	convert -trim demo-JHEP-mod.png ../docs/images/demo-JHEP-mod.png
